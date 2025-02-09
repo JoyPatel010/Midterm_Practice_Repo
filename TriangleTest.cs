e4d2e218411123eab250aba58269466093300263
Joy Patel <pateljoy149@gmail.com>
2025-02-09 16:51:09 -0500

    Joy added TestClass File

---
+++ TriangleTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MidTerm_Prep_Testing;
+using NUnit.Framework;
+
+
+
+namespace TestClass
+{
+    public class TriangleTest
+    {
+        [Test]
+
+        public void TriangleTest1_Input60and60and60_OutputValidTriangle()
+        {
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "This triangle is Valid";
+
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+            Assert.That(actual, Is.EqualTo(expected).IgnoreCase);
+
+
+        }
+
+        
+    }
+}
