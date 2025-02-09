cf55658b01ce72884db85c7c940c1d9434ff687a
Joy Patel <pateljoy149@gmail.com>
2025-02-09 18:07:46 -0500

    Added project

---
+++ MidTerm_Prep_Testing/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_Prep_Testing
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            string result;
+
+            if((firstAngle+secondAngle+thirdAngle) == 180)
+            {
+                result = "This triangle is valid";
+            }
+            else
+            {
+                result = "This triangle is NOT valid";
+            }
+
+            return result;
+        }
+    }
+}
